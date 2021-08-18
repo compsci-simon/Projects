79e3c1c2b31b380deb5139c59738390199ef3b5f
Simon Steven <21659583@sun.ac.za>
2021-08-18 14:32:13 +0200

    JRE 11 again

--- project1/ChatAppv2/src/ServerMain.java
+++ project1/ChatAppv2/src/ServerMain.java
@@ -1,6 +1,6 @@
 
 public class ServerMain {
 	public static void main(String[] args) {
-		Server server = new Server(9006);
+		Server server = new Server(9005);
 	}
 }
--- project1/ChatAppv2/src/Worker.java
+++ project1/ChatAppv2/src/Worker.java
@@ -16,23 +16,29 @@ public class Worker extends Thread {
 	@Override
 	public void run() {
 		try {
-			this.outStream = clientSocket.getOutputStream();
-			this.inStream = clientSocket.getInputStream();
-
-			outStream.write("Welcome to the server!\n".getBytes());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
-			String line;
-
-			while ((line = reader.readLine()) != null) {
-				if (line.equals("quit")) {
-					outStream.write("Succesfully logged off.".getBytes());
-					clientSocket.close();
-				} else {
-					outStream.write("Unrecognized command!\n".getBytes());
-				}
-			}
+			handle_Socket();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void handle_Socket() throws IOException {
+		this.outStream = clientSocket.getOutputStream();
+		this.inStream = clientSocket.getInputStream();
+
+		outStream.write("Welcome to the server!\n".getBytes());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+			if (line.equals("quit")) {
+				outStream.write("Succesfully logged off.\n".getBytes());
+				System.out.println("User logged off.");
+				break;
+			} else {
+				outStream.write("Unrecognized command!\n".getBytes());
+			}
+		}
+		clientSocket.close();
 	}
 }
