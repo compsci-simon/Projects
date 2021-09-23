755ba74ce854b89578aa105c9b09d0b5c703872b
Jaco Wynand Du Toit <22808892@sun.ac.za>
2021-09-23 12:36:12 +0200

    napt table basic functions setup

--- project3/natbox/natbox/src/main/java/nat/NAPT.java
+++ project3/natbox/natbox/src/main/java/nat/NAPT.java
@@ -1,5 +1,70 @@
 package nat;
 
+import java.util.HashMap;
+
 public class NAPT {
+	private HashMap<Long, byte[]> naptTable;
+
+	public NAPT() {
+		naptTable = new HashMap<Long, byte[]>();
+	}
+
+	public void addPair(byte[] addressIP, int port, byte[] externalIP, int externalPort) {
+		naptTable.put(toLong(addressIP, port), toBytes(externalIP, externalPort));
+	}
 	
+	public boolean containsSession(byte[] addressIP, int port) {
+		return naptTable.containsKey(toLong(addressIP, port));
+	}
+	  
+	public byte[] getMAC(byte[] addressIP, int port) {
+		return naptTable.get(toLong(addressIP, port));
+	}
+	
+	public long toLong(byte[] ipAddress, int port) {
+		byte[] bytes = new byte[8];
+		bytes[0] = (byte) ((port >> 24) & 0xff);
+		bytes[1] = (byte) ((port >> 16) & 0xff);
+		bytes[2] = (byte) ((port >> 8) & 0xff);
+		bytes[3] = (byte) (port & 0xff);
+		bytes[4] = ipAddress[0];
+		bytes[5] = ipAddress[1];
+		bytes[6] = ipAddress[2];
+		bytes[7] = ipAddress[3];
+		
+		long key = 0;
+		for (int i = 0; i < bytes.length; i++)
+		{
+		   key = (key << 8) + (bytes[i] & 0xff);
+		}
+		return key;
+	}
+	
+	public byte[] toBytes(byte[] ipAddress, int port) {
+		byte[] bytes = new byte[8];
+		bytes[0] = (byte) ((port >> 24) & 0xff);
+		bytes[1] = (byte) ((port >> 16) & 0xff);
+		bytes[2] = (byte) ((port >> 8) & 0xff);
+		bytes[3] = (byte) (port & 0xff);
+		bytes[4] = ipAddress[0];
+		bytes[5] = ipAddress[1];
+		bytes[6] = ipAddress[2];
+		bytes[7] = ipAddress[3];
+		
+		return bytes;
+	}
+	
+	public byte[] getAddress(byte[] bytes) {
+		byte[] ipAddress = new byte[4];
+		ipAddress[0] = bytes[4];
+		ipAddress[1] = bytes[5];
+		ipAddress[2] = bytes[6];
+		ipAddress[3] = bytes[7];
+		return ipAddress;
+	}
+	
+	public int getPort(byte[] bytes) {
+		return ((bytes[0] & 0xff) << 24) | ((bytes[1] & 0xff) << 16) | 
+		    ((bytes[2] & 0xff) << 8) | (bytes[3] & 0xff);
+	}
 }
